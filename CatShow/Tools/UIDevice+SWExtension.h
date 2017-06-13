6f6fd085554a19c9d871e39856160e42694e36d1
ysw-hello <shiwei_work@aliyun.com>
2017-06-13 17:00:32 +0800

    设备型号对应表，UIDevice-category

---
+++ CatShow/Tools/UIDevice+SWExtension.h
@@ -0,0 +1,20 @@
+//
+//  UIDevice+SWExtension.h
+//  CatShow
+//
+//  Created by Timmy on 2017/6/13.
+//  Copyright © 2017年 ysw.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIDevice (SWExtension)
+
+/**
+ 查询设备型号
+
+ @return 设备型号字符串
+ */
++(NSString *)deviceVersion;
+
+@end
